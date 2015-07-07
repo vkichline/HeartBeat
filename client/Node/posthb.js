b00ea313db65e2a88a0a53a431bbce82c94cfa7f
Van Kichline <vkichline@hotmail.com>
2015-07-07 16:45:19 -0700

    Fixed Node test script to POST JSON to add.  Does not update yet.

--- client/Node/posthb.js
+++ client/Node/posthb.js
@@ -1,36 +1,29 @@
 var querystring = require('querystring'),
   http = require('http'),
   apiHost = 'kichline-heartbeat.azurewebsites.net',
-  post_info = {
-    'group': 'Kichline.Kirkland',
-    'device': 'Surface',
-    'service': 'posthb',
-    'status': ''
-  };
+  post_data = "{'group': 'Kichline.Kirkland','device': 'Surface','service': 'posthb','status': '###'}";
 
 function PostBeat(statusMsg) {
-  post_info.status = statusMsg;
-  var post_options = {
-    host: apiHost,
-    port: '80',
-    path: '/api/HeartBeat?' + querystring.stringify(post_info),
-    method: 'POST',
-  };
-  //console.log(post_options.host+post_options.path);
-  var post_req = http.request(post_options, function (res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('Response: ' + chunk);
+    post_data = post_data.replace("###", statusMsg);
+    var post_options = {
+        host: apiHost,
+        path: '/api/HeartBeat',
+        port: 80,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+    var post_req = http.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
     });
-  });
-  post_req.write("");
-  post_req.end();
+
+    post_req.write(post_data);
+    post_req.end();
 }
 
-function KeepBeat(statusMsg) {
-  PostBeat(statusMsg);
-  return setInterval(PostBeat, 300000, statusMsg);
-}
-
-KeepBeat("UP");
-
+PostBeat("OK");
